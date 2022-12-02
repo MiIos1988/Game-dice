c98b42fffdd792648305d2284ec2fafe9a1117e0
Milos1988 <vojvoda1988@gmail.com>
2022-12-02 10:46:09 +0100

    finish all

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const name1 = document.querySelector(".pl1 > .name >h2")
 const name2 = document.querySelector(".pl2 > .name >h2")
 const roundTag = document.querySelector(".round")
+const win = document.querySelector(".win >h3")
 
+let maxRound = 3;
 let round = 1;
 
 const player1 = {
@@ -17,7 +19,6 @@ const player2 = {
     score: 0,
 }
 let dices = [
-
     null,
     "img/dice-1.png",
     "img/dice-2.png",
@@ -26,39 +27,26 @@ let dices = [
     "img/dice-5.png",
     "img/dice-6.png",
 ];
-
 let players = [player1, player2]
+let currentPlayer = null
+let playersIndex = null
 
+startGame()
 
 function random(max, min) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 function startGame() {
-    let whoPlaysFirst = players[random(2, 0)]
-
-    roundTag.innerText = round;
+    playersIndex = random(2, 0)
+    currentPlayer = players[playersIndex]
     name1.innerText = player1.name
     name2.innerText = player2.name
 
-    // if ((player1.allDice.querySelectorAll("img").length) === (player2.allDice.querySelectorAll("img").length)) {
-    //     round++
-    // }
-
-    rotateDice(whoPlaysFirst)
-
-   
-
+    rotateDice()
 }
 
-function saveThrow(score, player) {
-    player.score += score
-    player.allDice.innerHTML += `<img src="${dices[`${score}`]}">`;
-    if (player1.allDice)
-        startGame()
-}
-
-function rotateDice(player) {
+function rotateDice() {
     let numberImg = random(7, 1)
     let stopDice = setInterval(() => {
 
@@ -67,9 +55,9 @@ function rotateDice(player) {
     }, 100)
     setTimeout(() => {
         clearInterval(stopDice)
-        player.dice.setAttribute("src", dices[numberImg])
-        saveThrow(numberImg, player)
-
+        currentPlayer.dice.setAttribute("src", dices[numberImg])
+        saveThrow(numberImg)
+        // rotateDice(player)
 
     }, 3000
     )
@@ -111,7 +99,6 @@ function upRound() {
 }
 
 function startDice(player) {
-    console.log("object");
     let numberImg = random(7, 1)
     player.dice.setAttribute("src", `img/dice-${numberImg}.png`)
 }
