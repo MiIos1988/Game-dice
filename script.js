29f1c43098d88644c60b4252ffdf970120ba5336
Milos1988 <vojvoda1988@gmail.com>
2022-12-02 10:40:53 +0100

    finish

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const name1 = document.querySelector(".pl1 > .name >h2")
 const name2 = document.querySelector(".pl2 > .name >h2")
 const roundTag = document.querySelector(".round")
+const win = document.querySelector(".win >h3")
 
+let maxRound = 3;
 let round = 1;
 
 const player1 = {
@@ -27,60 +29,77 @@ let dices = [
 ];
 
 let players = [player1, player2]
+let currentPlayer = null
+let playersIndex = null
 
+startGame()
 
 function random(max, min) {
-
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 function startGame() {
-    let whoPlaysFirst = players[random(2, 0)]
-
-    roundTag.innerText = round;
+    playersIndex = random(2, 0)
+    currentPlayer = players[playersIndex]
     name1.innerText = player1.name
     name2.innerText = player2.name
 
-    // if ((player1.allDice.querySelectorAll("img").length) === (player2.allDice.querySelectorAll("img").length)) {
-    //     round++
-    // }
-
-    rotateDice(whoPlaysFirst)
-
-   
-
+    rotateDice()
 }
 
-function saveThrow(score, player) {
-    player.score += score
-    player.allDice.innerHTML += `<img src="${dices[`${score}`]}">`;
-    if (player1.allDice)
-        startGame()
-}
-
-function rotateDice(player) {
+function rotateDice() {
     let numberImg = random(7, 1)
     let stopDice = setInterval(() => {
 
-        player.dice.setAttribute("src", dices[random(7, 1)])
-        // player.score = numberImg;
+        currentPlayer.dice.setAttribute("src", dices[random(7, 1)])
 
     }, 100)
     setTimeout(() => {
         clearInterval(stopDice)
-        player.dice.setAttribute("src", dices[numberImg])
-        saveThrow(numberImg, player)
-
+        currentPlayer.dice.setAttribute("src", dices[numberImg])
+        saveThrow(numberImg)
+        // rotateDice(player)
 
     }, 3000
     )
+}
 
+function saveThrow(score) {
+    currentPlayer.score += score
+    currentPlayer.allDice.innerHTML += `<img src="${dices[`${score}`]}">`;
+
+    switchPlayer()
+
+    if (currentPlayer.allDice.querySelectorAll("img").length < maxRound) {
+        if (player1.allDice.querySelectorAll("img").length === player2.allDice.querySelectorAll("img").length) {
+            upRound()
+        }
+        rotateDice()
+    } else {
+        if (player1.score > player2.score) {
+            win.innerText = `Winner is ${player1.name} - ${player1.score}`
+        } else if (player2.score > player1.score) {
+            win.innerText = `Winner is ${player2.name} - ${player2.score}`
+        } else {
+            win.innerText = "No winner"
+        }
+    }
+}
+
+function switchPlayer() {
+    if (currentPlayer === player1) {
+        currentPlayer = player2
+    } else {
+        currentPlayer = player1
+    }
+}
+
+function upRound() {
+    round++
+    roundTag.innerText = round
 }
 
 function startDice(player) {
-    console.log("object");
     let numberImg = random(7, 1)
     player.dice.setAttribute("src", `img/dice-${numberImg}.png`)
 }
-
-startGame()
